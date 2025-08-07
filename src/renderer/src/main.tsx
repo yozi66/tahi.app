2c1f1f593c034b1f73fcf6de85defa37e461f44a
Jozsef Frigo <frigojozsef@gmail.com>
2025-08-07 06:56:49 +0200

    fix text color in light mode

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,9 @@
-import './assets/main.css'
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { TahiRouter } from './TahiRouter'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { TahiRouter } from './TahiRouter';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TahiRouter />
   </StrictMode>
-)
+);
