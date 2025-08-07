1b3949bf38b7f23ebb63eb301aacabb9c6ab42d8
Jozsef Frigo <frigojozsef@gmail.com>
2025-08-07 07:14:47 +0200

    chore: prittier

--- src/renderer/src/components/Todolist/Todolist.tsx
+++ src/renderer/src/components/Todolist/Todolist.tsx
@@ -1,31 +1,31 @@
-import '@mantine/core/styles.layer.css'
-import 'mantine-datatable/styles.layer.css'
+import '@mantine/core/styles.layer.css';
+import 'mantine-datatable/styles.layer.css';
 
-import { DataTable, DataTableColumn } from 'mantine-datatable'
-import { Text } from '@mantine/core'
-import { TahiState, TodoItem } from '../../TodoData'
-import { TodoContext } from  '../../TodoContext'
-import { useContext } from 'react'
+import { DataTable, DataTableColumn } from 'mantine-datatable';
+import { Text } from '@mantine/core';
+import { TahiState, TodoItem } from '../../TodoData';
+import { TodoContext } from '../../TodoContext';
+import { useContext } from 'react';
 
 export default function Todolist(): React.JSX.Element {
-  const { tahiState, setTahiState } = useContext(TodoContext)
+  const { tahiState, setTahiState } = useContext(TodoContext);
 
   const handleCellClick = (record: TodoItem, column: DataTableColumn<TodoItem>): void => {
-    const updatedState: TahiState = tahiState.shallowCopy()
+    const updatedState: TahiState = tahiState.shallowCopy();
 
-    const clickedItemId = record.id
-    updatedState.setSelectedItemId(clickedItemId)
-    const clickedIndex = updatedState.getSelectedItemIndex()
-    const updatedItems = updatedState.getTodoItems()
+    const clickedItemId = record.id;
+    updatedState.setSelectedItemId(clickedItemId);
+    const clickedIndex = updatedState.getSelectedItemIndex();
+    const updatedItems = updatedState.getTodoItems();
 
-    const oldSelectedItemIndex = tahiState.getSelectedItemIndex()
+    const oldSelectedItemIndex = tahiState.getSelectedItemIndex();
 
     // by default, stop editing the previous item
     if (oldSelectedItemIndex !== undefined) {
       updatedItems[oldSelectedItemIndex] = {
         ...updatedItems[oldSelectedItemIndex],
         title: { ...updatedItems[oldSelectedItemIndex].title, editing: false }
-      }
+      };
     }
 
     if (clickedIndex !== undefined) {
@@ -34,18 +34,18 @@ export default function Todolist(): React.JSX.Element {
         updatedItems[clickedIndex] = {
           ...updatedItems[clickedIndex],
           title: { ...updatedItems[clickedIndex].title, editing: true }
-        }
+        };
       }
       // Toggle the done state when the checkbox is clicked
       if (column.accessor === 'done') {
         updatedItems[clickedIndex] = {
           ...updatedItems[clickedIndex],
           done: !updatedItems[clickedIndex].done
-        }
+        };
       }
     }
-    setTahiState(updatedState)
-  }
+    setTahiState(updatedState);
+  };
 
   const handleInputChange = (record: TodoItem, newValue: string): void => {
     const tahiStateCopy = tahiState.shallowCopy();
@@ -55,34 +55,40 @@ export default function Todolist(): React.JSX.Element {
       title: { value: newValue, editing: true }
     });
     setTahiState(tahiStateCopy);
-  }
+  };
 
   const columns = [
     { accessor: 'id', title: 'ID' },
-    { accessor: 'done', title: 'Done', 
+    {
+      accessor: 'done',
+      title: 'Done',
       render: ({ done }: Pick<TodoItem, 'done'>) => (
-        <input type="checkbox" checked={done} readOnly /> 
-      ),
+        <input type="checkbox" checked={done} readOnly />
+      )
     },
-    { accessor: 'title', title: 'Title',
+    {
+      accessor: 'title',
+      title: 'Title',
       render: (todo: TodoItem) => {
         const title = todo.title;
         const chars = title.value.length;
         const width = chars < 20 ? '140px' : `${chars * 7}px`;
         return title.editing ? (
-          <input 
-            type="text" 
-            value={title.value} 
+          <input
+            type="text"
+            value={title.value}
             style={{ width: `${width}` }}
             onChange={(e) => handleInputChange(todo, e.target.value)}
             autoFocus
           />
         ) : (
-          <Text truncate='end' size='sm'>{title.value}</Text>
+          <Text truncate="end" size="sm">
+            {title.value}
+          </Text>
         );
-      },
+      }
     },
-    { accessor: 'comments', title: 'Comments', ellipsis: true,},
+    { accessor: 'comments', title: 'Comments', ellipsis: true }
   ];
 
   return (
@@ -91,10 +97,9 @@ export default function Todolist(): React.JSX.Element {
       columns={columns}
       withTableBorder
       highlightOnHover
-      onCellClick={({record, column}) => {
-          handleCellClick(record as TodoItem, column);
-        }
-      }
+      onCellClick={({ record, column }) => {
+        handleCellClick(record as TodoItem, column);
+      }}
       rowBackgroundColor={({ id }) =>
         tahiState.getSelectedItemId() === id ? { dark: '#444444', light: '#eeeeee' } : undefined
       }
--- src/renderer/src/components/TodolistSplit/TodolistSplit.tsx
+++ src/renderer/src/components/TodolistSplit/TodolistSplit.tsx
@@ -6,38 +6,41 @@ import { useContext } from 'react';
 
 export default function TodolistSplit(): React.JSX.Element {
   const { tahiState, setTahiState } = useContext(TodoContext);
-  
+
   function handleInputChange(value: string): void {
     const selectedItemIndex = tahiState.getSelectedItemIndex();
     const selectedItem = tahiState.getSelectedItem();
     if (!selectedItem || !selectedItemIndex) {
       return;
     }
-    const updatedItem = {...selectedItem, comments: value};
+    const updatedItem = { ...selectedItem, comments: value };
     const updatedState = tahiState.shallowCopy();
     updatedState.replaceItem(selectedItemIndex, updatedItem);
     setTahiState(updatedState);
   }
 
   return (
-    <Box h='calc(100vh - var(--app-shell-header-height) - 32px)' 
-      style={{display: 'flex', flex: 1 }}
+    <Box
+      h="calc(100vh - var(--app-shell-header-height) - 32px)"
+      style={{ display: 'flex', flex: 1 }}
     >
       <Allotment vertical>
-        <Allotment.Pane minSize={50} >
+        <Allotment.Pane minSize={50}>
           <Todolist />
         </Allotment.Pane>
-        <Allotment.Pane minSize={65} >
+        <Allotment.Pane minSize={65}>
           <Textarea
             label="Task comments"
             value={tahiState.getSelectedItem()?.comments || ''}
             styles={{
               root: { height: '100%' },
               input: { height: `calc(100% - 30px)`, resize: 'none' },
-              wrapper: { height: '100%' },
+              wrapper: { height: '100%' }
             }}
-            onChange={(e) => { handleInputChange(e.target.value); }}
-          /> 
+            onChange={(e) => {
+              handleInputChange(e.target.value);
+            }}
+          />
         </Allotment.Pane>
       </Allotment>
     </Box>
