567ea2c3ffc7b1cd44c178e235c25434acb19b20
Jozsef Frigo <frigojozsef@gmail.com>
2025-08-24 11:58:51 +0200

    remove unused test code

--- src/renderer/code/features/technology/Technology.page.tsx
+++ src/renderer/code/features/technology/Technology.page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ColorSchemeToggle } from './ColorSchemeToggle';
-import { Welcome } from './Welcome/Welcome';
+import { Welcome } from './Welcome';
 
 export function TechnologyPage(): React.JSX.Element {
   return (
